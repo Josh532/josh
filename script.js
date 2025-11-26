bbd5ab56200591dcc3d453c914f855feafa4a6d4
Dukenukem <joshmckenzie619@gmail.com>
2025-11-26 23:59:41 +1300

    Update script.js

--- script.js
+++ script.js
@@ -5,4 +5,19 @@ const offScreenMenu = document.querySelector(".off-screen-menu");
 menu.addEventListener("click", () => {
   menu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
+
 });
+
+
+
+  window.onload = function() {
+    const currentURL = window.location.href;
+    const lowerCaseURL = currentURL.toLowerCase();
+
+
+    if (currentURL !== lowerCaseURL) {
+
+      window.location.replace(lowerCaseURL);
+    }
+  };
+</script>
